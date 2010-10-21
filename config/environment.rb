f4433d855a942f87feba240ba08bee0c395ab227
kalpesh dave <kdave@fourthmedia.co.uk>
2010-10-21 18:36:51 +0530

    environment

--- config/environment.rb
+++ config/environment.rb
@@ -19,7 +19,7 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
-    
+    config.gem "rack", :version => "1.1.0"
 #    config.gem "rubyzip"
     # Testing Gems
 #    config.gem 'rspec',       :lib => false
